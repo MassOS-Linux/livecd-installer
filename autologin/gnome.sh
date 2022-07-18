99beee090bfd96d4a65a03e96443e98598056745
Daniel Massey <masseyd1396@gmail.com>
2022-07-18 17:15:53 +0100

    Fix GNOME autologin

--- autologin/gnome.sh
+++ autologin/gnome.sh
@@ -1,3 +1,3 @@
 cp iso-workdir/massos-rootfs/etc/gdm/custom.conf{,.orig}
-sed -i '6iAutomaticLoginEnable=True' /etc/gdm/custom.conf
-sed -i '7iAutomaticLogin=massos' /etc/gdm/custom.conf
+sed -i '6iAutomaticLoginEnable=True' iso-workdir/massos-rootfs/etc/gdm/custom.conf
+sed -i '7iAutomaticLogin=massos' iso-workdir/massos-rootfs/etc/gdm/custom.conf
